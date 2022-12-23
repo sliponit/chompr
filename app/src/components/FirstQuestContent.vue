b65b7e8b519fb0a374983b4cafa9f5a1a1ce83f3
sliponit <sliponit@pm.me>
2022-12-23 07:29:45 +0100

    feat: email login

--- app/src/components/FirstQuestContent.vue
+++ app/src/components/FirstQuestContent.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
     <h1>First Quest</h1>
-    <div v-if="!doneClick">
-      <p>{{ `You have ${profile?.public_metrics?.tweet_count} tweets` }}</p>
+    <div v-if="!clicked.first">
+      <input
+        v-model="handle"
+        placeholder="edit me" />
+      <button class="btn btn-primary mt-5" @click="fetchAndClicked">Validate</button>
+    </div>
+    <div v-else-if="!clicked.second">
+      <p>{{ `You have ${profile?.public_metrics?.tweet_count} tweets for your ${handle} account` }}</p>
       <p>Go clean some, see eg <a href="https://www.jeffbullas.com/twitter-tools-to-delete-tweets/" target="_blank">this blog post</a> for a list of automated tools</p>
-      <button class="btn btn-primary mt-5" @click="fetchApiAndUpdateRef">Done</button>
+      <button class="btn btn-primary mt-5" @click="() => fetchAndClicked(false)">Done</button>
     </div>
 
     <div class="result-block-container">
-      <div :class="['result-block', doneClick ? 'show' : '']">
+      <div :class="['result-block', clicked.second ? 'show' : '']">
         <h6 class="muted">{{ `Congrats! You now have ${profile?.public_metrics?.tweet_count} tweets` }}</h6>
         <highlightjs language="json" :code="JSON.stringify(profile, null, 2) || ''" />
       </div>
@@ -28,24 +34,19 @@ function fetchApi (name) {
 
 export default {
   name: "first-quest-view",
-  props: {
-    name: String,
-  },
-  async setup(props) {
-    const doneClick = ref(false)
-    const profile = ref();
-    profile.value = await fetchApi(props.name)
+  setup() {
+    const clicked = ref({ first: false, second: false })
+    const handle = ref('')
+    const profile = ref()
     return {
-      doneClick,
+      clicked,
+      handle,
       profile,
-      async fetchApiAndUpdateRef() {
-        try {
-          profile.value = await fetchApi(props.name);
-          doneClick.value = true
-        } catch (e) {
-          profile.value = 'error' // TODO?? `Error: the server responded with '${e.response.status}: ${e.response.statusText}'`;
-        }
-      },
+      async fetchAndClicked(isFirst = true) {
+        profile.value = await fetchApi(handle.value);
+        if (isFirst) clicked.value.first = true
+        else clicked.value.second = true
+      }
     };
   },
 };
--- app/src/views/FirstQuest.vue
+++ app/src/views/FirstQuest.vue
@@ -1,23 +1,14 @@
 <template>
   <div>
-    <Suspense>
-      <first-quest-content :name="user?.name" />
-    </Suspense>
+    <first-quest-content />
   </div>
 </template>
 
 <script>
-import { useAuth0 } from '@auth0/auth0-vue';
 import FirstQuestContent from '../components/FirstQuestContent.vue';
 
 export default {
   components: { FirstQuestContent },
   name: "first-quest-view",
-  setup() {
-    const auth0 = useAuth0();
-    return {
-      user: auth0.user,
-    };
-  },
 };
 </script>
